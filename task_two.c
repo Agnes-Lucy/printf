31ec11705115e3558f59b2c7e422b9e3cf13c5f8
Agnes-Lucy <udohlucee@yahoo.com>
2023-12-06 00:42:38 -0800

    task2

--- task_two.c
+++ task_two.c
@@ -1,92 +1,38 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _printf - a function that produces output according to a format
+ * @format: a pointer to a format string containing conversion specifiers
  * @count - count the number of character printed
  * @c - character
- * Return: 0
+ * Return: count
 **/
 
-int _printf(const char *format, ...) 
+int _printf(const char *format, ...)
 {
 	int count;
 	va_list args;
+
 	va_start(args, format);
 
 	count = 0;
 
 	while (*format != '\0')
 	{
-		if (*format == '%')
+		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
 		{
-			format++;
-			switch (*format) 
+			int num = va_arg(args, int);
+
+			count += printf("%d", num);
+			 format++;
+		} else
 			{
-				 case 'c': {
-						   char c = (char)va_arg(args, int);
-						   putchar(c);
-						   count++;
-						   break;
-					   }
-				case 's': {
-						  const char *s = va_arg(args, const char *);
-						  while (*s != '\0')
-						  {
-							  putchar(*s);
-							  s++;
-							  count++;
-						  }
-						  break;
-					  }
-				case '%':
-					  putchar('%');
-					  count++;
-					  break;
-				case 'd':
-					   case 'i': {
-							     int num = va_arg(args, int);
-							     printf("%d", num);
-							     count++;
-							     break;
-						     }
-				           case 'u': {
-							     unsigned int num = va_arg(args, unsigned int);
-							     printf("%u", num);
-							     count++;
-							     break;
-						      }
-					   case 'o': {
-							      unsigned int num = va_arg(args, unsigned int);
-							      printf("%o", num);
-							      count++;
-							      break;
-						     }
-				case 'x':
-					   case 'X': {
-							      unsigned int num = va_arg(args, unsigned int);
-							      printf("%x", num);
-							      count++;
-							      break;
-						     }
-				           case 'p': {
-							     void *ptr = va_arg(args, void *);
-							     printf("%p", ptr);
-							     count++;
-							     break;
-						     }
-				default:
-						 putchar('%');
-						 putchar(*format);
-						 count += 2;
-						 break;
+				count += putchar(*format);
 			}
-		} else {
-			putchar(*format);
-			count++;
-		}
 		format++;
 	}
 	va_end(args);
-	return count;
+	return (count);
 }
